bca2a94961907d259d0005e1f6d3f0887dc43cd0
gonzadavr <gonzadavr@users.noreply.github.com>
2022-07-07 21:43:52 -0300

    filtros

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -7,6 +7,8 @@ import { todoList } from "..";
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
 const btnBorrar =  document.querySelector('.clear-completed');
+const ulFilters = document.querySelector('.filters');
+const anchorFiltros =  document.querySelectorAll('.filtro');
 
 export const crearTodoHTML = (todo)=>{
 
@@ -89,4 +91,39 @@ btnBorrar.addEventListener('click', (evento)=>{
     
 
 
+});
+
+ulFilters.addEventListener('click', (evento)=>{
+
+
+    console.log(evento.target.text);
+    const filtro = evento.target.text;
+    if(!filtro) return;
+
+    console.log(anchorFiltros);
+    anchorFiltros.forEach(elemento => elemento.classList.remove('selected'));
+    
+    evento.target.classList.add('selected');
+
+
+    for(const elemento of divTodoList.children){
+    
+        elemento.classList.remove('hidden');
+        const completado =  elemento.classList.contains('completed');
+
+        switch(filtro){
+
+            case 'Pendientes':
+                if(completado){
+                    elemento.classList.add('hidden');
+                }
+            break;    
+
+            case 'Completados':
+                if(!completado){
+                    elemento.classList.add('hidden');
+                }
+            break; 
+        }
+    }
 });
